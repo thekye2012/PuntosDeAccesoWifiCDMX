d4abc88375d8d7f03e200274d134d6cc0d061722
Gerardo <thekye2012@hotmail.com>
2024-02-15 13:46:29 -0600

    Initial commit

---
+++ src/main/java/com/promotores/puntosdeaccesowificdmx/PuntosDeAccesoWifiCdmxApplication.java
@@ -0,0 +1,13 @@
+package com.promotores.puntosdeaccesowificdmx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PuntosDeAccesoWifiCdmxApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PuntosDeAccesoWifiCdmxApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/promotores/puntosdeaccesowificdmx/PuntosDeAccesoWifiCdmxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.promotores.puntosdeaccesowificdmx;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PuntosDeAccesoWifiCdmxApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
